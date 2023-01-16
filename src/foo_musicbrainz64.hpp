7783326fcbe73ddcb013c56972e6a22bad272752
marc2k3 <marc2003@live.co.uk>
2023-01-16 04:38:09 +0000

    1.2.2

--- src/foo_musicbrainz64.hpp
+++ src/foo_musicbrainz64.hpp
@@ -3,6 +3,6 @@
 namespace Component
 {
 	static constexpr wil::zstring_view name = "MusicBrainz64";
-	static constexpr wil::zstring_view version = "1.2.1";
+	static constexpr wil::zstring_view version = "1.2.2";
 	static constexpr wil::zstring_view home_page = "https://marc2k3.github.io/component/musicbrainz64/";
 }
