a2eea8de2cbb0d852e16970b47cf34001d45ae4b
marc2k3 <marc2003@live.co.uk>
2023-04-01 06:41:33 +0100

    1.2.7

--- src/foo_musicbrainz64.hpp
+++ src/foo_musicbrainz64.hpp
@@ -3,6 +3,6 @@
 namespace Component
 {
 	static constexpr std::string_view name = "MusicBrainz64";
-	static constexpr std::string_view version = "1.2.6";
+	static constexpr std::string_view version = "1.2.7";
 	static constexpr std::string_view home_page = "https://marc2k3.github.io/component/musicbrainz64/";
 }
